2771957232ad80688d6a64facfe6e2fc1af52f98
0hasanov <emilhesenov955@gmail.com>
2022-05-14 15:59:38 +0400

    Tasks

--- eOlmypTasks/src/PP1Week2Contest2/DivisibilityByDigits.java
+++ eOlmypTasks/src/PP1Week2Contest2/DivisibilityByDigits.java
@@ -7,9 +7,9 @@ public class DivisibilityByDigits {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int firstdigit  = a /1000 %10;
-        int seconddigit = a /1000 %10;
-        int thirddigit = a /1000 %10;
-        int fourthdigit = a /1000 %10;
+        int seconddigit = a /100 %10;
+        int thirddigit = a /10 %10;
+        int fourthdigit = a  % 10;
         if (a % firstdigit ==0 && a % seconddigit ==0 && a % thirddigit ==0 && a % fourthdigit ==0) {
             System.out.println("YES");
         }
