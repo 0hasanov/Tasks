37d554d1f894f8f61d1a47741359088ac2dd6507
0hasanov <emilhesenov955@gmail.com>
2022-05-17 03:23:50 +0400

    Task

---
+++ eOlmypTasks/src/Exercises/LoopExercise1.java
@@ -0,0 +1,26 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class LoopExercise1 {
+    public static void main(String[] args) {
+        int a, i, number;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the Value of n: ");
+        a = scan.nextInt();
+        System.out.print("Enter " + a + " Numbers: ");
+        for (i = 0; i < a; i++) {
+            number = scan.nextInt();
+            System.out.println(number);
+            if (a > 0) {
+                System.out.println(number + " is positive");
+            } else if (a < 0) {
+                System.out.println(number + " is negative");
+            } else if (a == 0) {
+
+                System.out.println(number + " is zero");
+            }
+        }
+    }
+}
