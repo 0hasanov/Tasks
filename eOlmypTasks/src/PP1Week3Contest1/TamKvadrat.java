f46d8a5ea76b43cbaf642ed6a8b60f95de057ada
0hasanov <emilhesenov955@gmail.com>
2022-05-17 13:32:21 +0400

    Task

---
+++ eOlmypTasks/src/PP1Week3Contest1/TamKvadrat.java
@@ -0,0 +1,16 @@
+package PP1Week3Contest1;
+
+import java.util.Scanner;
+
+public class TamKvadrat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = (int) Math.sqrt(n);
+        if (n == Math.pow(m, 2)) {
+            System.out.println(m);
+        } else {
+            System.out.println("No");
+        }
+    }
+}
