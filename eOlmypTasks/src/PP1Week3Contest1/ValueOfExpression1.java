c5467c5381e03b5a244a7a25161ac582313ef9b4
0hasanov <emilhesenov955@gmail.com>
2022-05-17 13:24:10 +0400

    Task

---
+++ eOlmypTasks/src/PP1Week3Contest1/ValueOfExpression1.java
@@ -0,0 +1,13 @@
+package PP1Week3Contest1;
+
+import java.util.Scanner;
+
+public class ValueOfExpression1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double s = 2 * Math.pow(x, 2) - 4 * x * y + 3 * Math.pow(y, 2) + (x + y) / 7;
+        System.out.printf("%.3f",s);
+    }
+}
