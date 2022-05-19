b1f5e7976361285e486cf665e66862a0fb47e731
0hasanov <emilhesenov955@gmail.com>
2022-05-20 02:13:31 +0400

    Task

---
+++ eOlmypTasks/src/linearArrays/EvenIndexes.java
@@ -0,0 +1,19 @@
+package linearArrays;
+
+import java.util.Scanner;
+
+public class EvenIndexes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        for (int i = 0; i < n; i=i+2) {
+            System.out.print(arr[i] + "\t");
+
+        }
+    }
+}
