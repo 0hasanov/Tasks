fe7699c55fde605b9da629db8a315ccde72515e3
Task <emilhesenov955@gmail.com>
2022-05-21 18:50:59 +0400

    task

--- eOlmypTasks/src/linearArrays/BiggerThanPrevious.java
+++ eOlmypTasks/src/linearArrays/BiggerThanPrevious.java
@@ -11,7 +11,7 @@ public class BiggerThanPrevious {
             arr[i] = sc.nextInt();
 
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             if (arr[i] > arr[i - 1]) {
                 System.out.print("arr[i]" + "\t");
 
