768047b0ebf19e21014d82e7679b4abcd9fb0d48
0hasanov <emilhesenov955@gmail.com>
2022-05-24 14:38:47 +0400

    Task

---
+++ eOlmypTasks/src/exercises/stringex.java
@@ -0,0 +1,18 @@
+/*package exercises;
+
+import java.util.Scanner;
+
+public class stringex {
+        public static void main(String[] args) {
+            Scanner console = new Scanner(System.in);
+            System.out.print("a");
+
+            if (num > 0) {
+                for (int i = 0; i < num; i++) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
+*/
--- eOlmypTasks/src/pp1Week3Contest1/Armstronq.java
+++ eOlmypTasks/src/pp1Week3Contest1/Armstronq.java
@@ -6,23 +6,25 @@ public class Armstronq {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int[] arr = new int[a];
+        int b = sc.nextInt();
         int firstdigit = a / 1000;
         int seconddigit = a / 100 % 10;
         int thirddigit = a / 10 % 10;
         int lastdigit = a % 10;
-        for (int i = 1000; i <= arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int tr = (int) ((Math.pow(firstdigit, 4) + Math.pow(seconddigit, 4) + Math.pow(thirddigit, 4) + Math.pow(lastdigit, 4)));
-        for (int number : arr) {
-            if (number == tr) {
-                number = tr;
+        for (int i = a; i <= b; i++) {
+
+            int tr = (int) ((Math.pow(firstdigit, 4) + Math.pow(seconddigit, 4) + Math.pow(thirddigit, 4) + Math.pow(lastdigit, 4)));
+
+            for (int number = 0; number >= tr; number++) {
+                if (number == tr) {
+                    number = tr;
+                }
+
             }
+            System.out.println(tr);
 
         }
-        System.out.println(Math.pow(firstdigit, 4) + Math.pow(seconddigit, 4) + Math.pow(thirddigit, 4) + Math.pow(lastdigit, 4));
 
     }
-
 }
+
