d700db0127d5a0ba2a760fd93e5d9ec416b16415
0hasanov <emilhesenov955@gmail.com>
2022-06-18 16:12:37 +0400

    Exercises

---
+++ eOlmypTasks/src/pp1AndABitmore/Rum.java
@@ -0,0 +1,38 @@
+package pp1AndABitmore;
+
+import java.util.Scanner;
+
+public class Rum {
+
+
+    public static int rumToInt(String rum) {
+
+        int number = 0;
+        for (int i = 0; i < rum.length(); i++) {
+            char c = rum.charAt(i);
+            if (c == 'I') {
+                number += 1;
+            } else if (c == 'V') {
+                number += 5;
+            } else if (c == 'X') {
+                number += 10;
+            } else if (c == 'L') {
+                number += 50;
+            } else if (c == 'C') {
+                number += 100;
+            } else if (c == 'D') {
+                number += 500;
+            } else if (c == 'M') {
+                number += 1000;
+            }
+        }
+
+        return number;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String rum = String.valueOf(rumToInt(sc.next()));
+        System.out.println(rum);
+    }
+}
