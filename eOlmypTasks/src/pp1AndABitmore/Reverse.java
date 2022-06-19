cfa6357a6db48f65b1b8c53aef7ed67b2e93f60d
0hasanov <emilhesenov955@gmail.com>
2022-06-19 16:42:42 +0400

    Reverse

--- eOlmypTasks/src/pp1AndABitmore/Reverse.java
+++ eOlmypTasks/src/pp1AndABitmore/Reverse.java
@@ -2,7 +2,7 @@ package pp1AndABitmore;
 
 import java.util.Scanner;
 
-public class reverse {
+public class Reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
