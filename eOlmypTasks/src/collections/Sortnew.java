7eba6e66ea85edd502c3766474b94c9fe332f512
Task <emilhesenov955@gmail.com>
2022-06-25 11:57:03 +0400

    task

---
+++ eOlmypTasks/src/collections/Sortnew.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Sortnew {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(sc.nextInt());
+        number.add(sc.nextInt());
+        number.add(sc.nextInt());
+
+
+    }
+    public static int ascendMethod(ArrayList<Integer>rlist){
+        int ascend = rlist.get(0);
+        for (int number:
+             rlist) {
+            if ()
+
+        }
+    }
+
+
+}
