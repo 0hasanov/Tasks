b4bdde35a5db5101297ea23db0447338447404a4
0hasanov <emilhesenov955@gmail.com>
2022-06-30 15:01:44 +0400

    COLLECTIONS

--- eOlmypTasks/src/collections/two/Main.java
+++ eOlmypTasks/src/collections/two/Main.java
@@ -6,6 +6,8 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("First version");
+
         User coder = new User();
         Set<User> user1 = new HashSet<>();
         user1.add(coder);
@@ -36,10 +38,68 @@ public class Main {
         System.out.println(user4);
 
 
+        System.out.println("Delete same ");
+
+        if (coder.getName() == teacher.getName() || coder.getSurname() == teacher.getSurname()) {
+            user1.clear();
+            user2.clear();
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+        }
+
+        if (coder.getName() == doctor.getName() || coder.getSurname() == doctor.getSurname()) {
+            user1.clear();
+            user3.clear();
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+        }
+
+        if (coder.getName() == artist.getName() || coder.getSurname() == artist.getSurname()) {
+            user1.clear();
+            user4.clear();
+
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+        }
+
+        if (teacher.getName() == doctor.getName() || teacher.getSurname() == doctor.getSurname()) {
+            user2.clear();
+            user3.clear();
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+        }
+
+        if (teacher.getName() == artist.getName() || teacher.getSurname() == artist.getSurname()) {
+            user2.clear();
+            user4.clear();
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+        }
+
+        if (doctor.getName() == artist.getName() || doctor.getSurname() == artist.getSurname()) {
+            user3.clear();
+            user4.clear();
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+        }
+
+
     }
-
-
 }
+
+
 
 
 
