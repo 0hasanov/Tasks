42046a02b3e143a67d91e7fa642ae1d8c6230ad1
0hasanov <emilhesenov955@gmail.com>
2022-06-30 15:48:00 +0400

    COLLECTIONS

--- eOlmypTasks/src/collections/two/DublicateMap.java
+++ eOlmypTasks/src/collections/two/DublicateMap.java
@@ -1,39 +1,36 @@
 package collections.two;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DublicateMap {
     public static void main(String[] args) {
 
-        Set<Integer> store = new HashSet<>();
-        store.add(1);
-        store.add(2);
-        store.add(4);
-        store.add(6);
-        store.add(5);
-        store.add(2);
-        store.add(1);
-        store.add(5);
-        Map<String, Integer> nameAndCount = new HashMap<>();
-        for (Integer name : store) {
-            Integer count = nameAndCount.get(name);
-            if (count == null) {
-                nameAndCount.put(String.valueOf(name), 1);
-            } else {
-                nameAndCount.put(String.valueOf(name), ++count);
-            }
-        }
-        Set<Map.Entry<String, Integer>> entrySet = nameAndCount.entrySet();
-        for (Map.Entry<String, Integer> entry : entrySet) {
-            if (entry.getValue() > 1) {
-                System.out.println(entry.getKey());
-            }
+
+        List<Integer> numbers = new ArrayList<>();
+
+
+        numbers.add(11);
+        numbers.add(22);
+        numbers.add(33);
+        numbers.add(44);
+        numbers.add(11);
+        numbers.add(55);
+        numbers.add(33);
+
+        for (Integer names : numbers) {
+            System.out.println(names);
         }
 
 
+        Map<Integer, Integer> count = new HashMap<>();
+
+
+        for (Integer names : numbers) {
+            count.put(names, count.getOrDefault(names, 0) + 1);
+        }
+
+
+        System.out.println(count);
     }
 }
 
