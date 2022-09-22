4bde8f7ad1197d8bd58a74e319cc7ebf921d1644
0hasanov <emilhesenov955@gmail.com>
2022-09-23 03:21:38 +0400

    metaVerse

--- Tasks/src/sql/MetaVerse.sql
+++ Tasks/src/sql/MetaVerse.sql
@@ -5,17 +5,16 @@ gender  varchar(255) not null,
 brand  varchar(255) not null,
 body  varchar(255) not null,
 colour  varchar(255) not null,
-price int ,
-created date default now()
+price int
 );
 
 create table Shoes
 (
+id int primary key,
 gender  varchar(255) not null,
 brand  varchar(255) not null,
 size  varchar(255) not null,
+body  varchar(255) not null,
 colour  varchar(255) not null,
-price int ,
-created date default now()
+price int
 );
-
